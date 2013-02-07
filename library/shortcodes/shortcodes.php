6351582c6fad588bbe732bc2c70de1dad46c6294
Sean C. Davis <scdavis41@gmail.com>
2013-02-07 07:37:57 -0500

    Added new icons, changed the accent-1 color to be more emerald. Added 'tile' shortcode and styled appropriately (no options).

--- library/shortcodes/shortcodes.php
+++ library/shortcodes/shortcodes.php
@@ -11,5 +11,17 @@ function build_column_shortcode( $atts, $content = null ) {
  	 if( $new_line == false ) return '<div class="col-' . esc_attr($number) . ' rt-col">' . do_shortcode($content) . '</div>';
 	 else return '<div class="col-' . esc_attr($number) . ' rt-col" style="clear:both;">' . do_shortcode($content) . '</div>';
 }
+
+add_shortcode( 'tile', 'build_tile_shortcode' );
+
+function build_tile_shortcode( $atts, $content = null ) {
+   extract( shortcode_atts( array(
+      'number' => '1-2',
+	  'new_line' => false
+      ), $atts ) );
+ 
+ 	 if( $new_line == false ) return '<div class="tile-' . esc_attr($number) . ' rt-tile">' . do_shortcode($content) . '</div>';
+	 else return '<div class="tile-' . esc_attr($number) . ' rt-tile" style="clear:both;">' . do_shortcode($content) . '</div>';
+}
 	
 ?>
