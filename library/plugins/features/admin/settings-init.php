cb303218a3d912a9d50afc0ef09434889426194c
Sean C. Davis <scdavis41@gmail.com>
2013-02-16 16:12:10 -0500

    Features settings page is dynamic. All content is pulled from one array, though some page elements have to be added manually. Will be cleaned up with function arguments added before distributing.

--- library/plugins/features/admin/settings-init.php
+++ library/plugins/features/admin/settings-init.php
@@ -5,53 +5,16 @@ function rt_register_feature_settings() {
 	add_submenu_page('edit.php?post_type=rt_feature', 'Feature Settings', 'Settings', 'manage_options', 'rt_feature_settings', 'rt_features_page');
 }
 
-
-/* Settings Control
--------------------------------------------------------------------------------- */
 add_action('admin_init', 'rt_feature_admin_init');
-
-/**
- * Registration of each setting field and section
- */
- 
-function rt_feature_admin_init(){
-	
-	/* Registers entire page of settings (rt_features)
-	-------------------------------- */
+function rt_feature_admin_init(){	
+	global $rt_feat_settings;
 	register_setting( 'rt_features', 'rt_features', 'rt_validate_feat_options' );
-	
-	
-	// Setup
-	add_settings_section( 'feat_settings_setup', '', 'feat_settings_setup', 'feat_settings_setup' );
-	
-	$section_name = 'feat_container_settings';	
-	add_settings_section( $section_name, '', $section_name, $section_name );	
-	add_settings_field('slider_width', 'Slider Width:', 'setdisp_slider_width', $section_name, $section_name);
-	add_settings_field('bkg_type', 'Background Type:', 'setdisp_bkg_type', $section_name, $section_name);
-	add_settings_field('bkg_color', 'Background Color:', 'setdisp_bkg_color', $section_name, $section_name);
-	
-	$section_name = 'feat_content_settings';
-	add_settings_section( $section_name, '', $section_name, $section_name );
-	add_settings_field('text_color', 'Text Color:', 'setdisp_text_color', $section_name, $section_name);
-	add_settings_field('linked_content', 'What do you want to use as a hyperlink?', 'setdisp_linked_content', $section_name, $section_name);
-	add_settings_field('link_new_window', 'Open links in new window?', 'setdisp_link_new_window', $section_name, $section_name);
-	add_settings_field('button_visible', 'Enable Call To Action Buttons:', 'setdisp_button_visible', $section_name, $section_name);
-	add_settings_field('button_bkg_color', 'Call To Action Button Color', 'setdisp_button_bkg_color', $section_name, $section_name);
-	add_settings_field('button_text_color', 'Call To Action Button TEXT Color', 'setdisp_button_text_color', $section_name, $section_name);
-	
-	$section_name = 'feat_slider_settings';
-	add_settings_section( $section_name, '', $section_name, $section_name );
-	add_settings_field('counter_type', 'Counter Type:', 'setdisp_counter_type', $section_name, $section_name);
-	add_settings_field('inactive_counter_color', 'Inactive Counter Color:', 'feat_color_field', $section_name, $section_name, 'inactive_counter_color');
-	add_settings_field('active_counter_color', 'Active Counter Color:', 'setdisp_active_counter_color', $section_name, $section_name);
-	add_settings_field('hover_counter_color', 'Hover Counter Color:', 'setdisp_hover_counter_color', $section_name, $section_name);
-	add_settings_field('inactive_counter_text_color', 'Inactive Counter Color:', 'setdisp_inactive_counter_text_color', $section_name, $section_name);
-	add_settings_field('active_counter_text_color', 'Active Counter Color:', 'setdisp_active_counter_text_color', $section_name, $section_name);
-	add_settings_field('hover_counter_text_color', 'Hover Counter Color:', 'setdisp_hover_counter_text_color', $section_name, $section_name);
-	add_settings_field('arrows_type', 'Arrows Type:', 'setdisp_arrows_type', $section_name, $section_name);
-	add_settings_field('arrows_bkg_color', 'Arrows Background Color:', 'setdisp_arrows_bkg_color', $section_name, $section_name);		 		 		 
-
+	foreach ($rt_feat_settings as $sections => $section) {
+		add_settings_section($sections, '', $sections, $sections);
+		foreach($section as $field) {
+			add_settings_field($field['name'], $field['label'], 'feat_' . $field['type'] . '_field', $sections, $sections, $field['name']);
+		}
+	}	
 }
-
 
 ?>
--- library/plugins/features/admin/settings-values.php
+++ library/plugins/features/admin/settings-values.php
@@ -1,71 +1,149 @@
-<?php
- 
-/* Set Default Values and return option value
--------------------------------------------------------------------------------- */
-function get_feat_option( $option_name, $choices = false ) {
-	
-	$options = array(
+<?php 
+
+global $rt_feat_settings;
+
+$rt_feat_settings = array(
+	'feat_container_settings' => array(
 		'slider_width' => array(
+			'name' => 'slider_width',
+			'label' => 'Slider Width:',
+			'type' => 'radio',
 			'default' => 'standard',
 			'choices' => array('standard', 'full-width')
 		),
 		'bkg_type' => array(
+			'name' => 'bkg_type',
+			'label' => 'Background Type:',
+			'type' => 'radio',
 			'default' => 'color',
 			'choices' => array('color','gradient','none')
 		),
-		'bkg_color' => array('default' => '#fff'),
-		'text_color' => array('default' => '#fff'),
+		'bkg_color' => array(
+			'name' => 'bkg_color',
+			'label' => 'Background Color:',
+			'type' => 'color',
+			'default' => '#fff'
+		),
+	),
+	'feat_content_settings' => array(
+		'text_color' => array(
+			'name' => 'text_color',
+			'label' => 'Text Color:',
+			'type' => 'color',
+			'default' => '#fff'
+		),
 		'linked_content' => array(
+			'name' => 'linked_content',
+			'label' => 'Linked Content:',
+			'type' => 'radio',
 			'default' => 'none',
 			'choices' => array('content', 'button', 'none')
 		),
-		'link_new_window' => array('default' => false),
-		'button_visible' => array('default' => false),
-		'button_bkg_color' => array('default' => '#000'),
-		'button_text_color' => array('default' => '#fff'),
+		'link_new_window' => array(
+			'name' => 'link_new_window',
+			'label' => 'Link New Window:',
+			'type' => 'checkbox',
+			'default' => false
+		),
+		'button_visible' => array(
+			'name' => 'button_visible',
+			'label' => 'Button Visible:',
+			'type' => 'checkbox',
+			'default' => false
+		),
+		'button_bkg_color' => array(
+			'name' => 'button_bkg_color',
+			'label' => 'Background Color:',
+			'type' => 'color',
+			'default' => '#000'
+		),
+		'button_text_color' => array(
+			'name' => 'button_text_color',
+			'label' => 'Button Text Color:',
+			'type' => 'color',
+			'default' => '#fff'
+		)
+	),
+	'feat_slider_settings' => array(
 		'counter_type' => array(
+			'name' => 'counter_type',
+			'label' => 'Counter Type:',
+			'type' => 'radio',
 			'default' => 'circles',
-			'choices' => array('circles','numbers','none')
+			'choices' => array('circles', 'numbers', 'none')
 		),
-		'inactive_counter_color' => array('default' => '#fff'),
-		'active_counter_color' => array('default' => '#000'),
-		'hover_counter_color' => array('default' => '#000'),
-		'inactive_counter_text_color' => array('default' => '#000'),
-		'active_counter_text_color' => array('default' => '#fff'),
-		'hover_counter_text_color' => array('default' => '#fff'),
+		'inactive_counter_color' => array(
+			'name' => 'inactive_counter_color',
+			'label' => 'Inactive Counter Color:',
+			'type' => 'color',
+			'default' => '#fff'
+		),
+		'active_counter_color' => array(
+			'name' => 'active_counter_color',
+			'label' => 'Active Counter Color:',
+			'type' => 'color',
+			'default' => '#000'
+		),
+		'hover_counter_color' => array(
+			'name' => 'hover_counter_color',
+			'label' => 'Hover Counter Color:',
+			'type' => 'color',
+			'default' => '#000'
+		),
+		'inactive_counter_text_color' => array(
+			'name' => 'inactive_counter_text_color',
+			'label' => 'Inactive Counter TEXT Color:',
+			'type' => 'color',
+			'default' => '#000'
+		),
+		'active_counter_text_color' => array(
+			'name' => 'active_counter_text_color',
+			'label' => 'Active Counter TEXT Color:',
+			'type' => 'color',
+			'default' => '#fff'
+		),
+		'hover_counter_text_color' => array(
+			'name' => 'hover_counter_text_color',
+			'label' => 'Hover Counter TEXT Color:',
+			'type' => 'color',
+			'default' => '#fff'
+		),
 		'arrows_type' => array(
+			'name' => 'arrows_type',
+			'label' => 'Arrows Type',
+			'type' => 'radio',
 			'default' => 'small',
-			'choices' => array('small','full-height','none')
+			'choices' => array('small', 'full-height', 'none')
 		),
-		'arrows_bkg_color' => array('default' => '#000')
-	);
-	
-	$current_options = get_option( 'rt_features' );
-	
-	if( $choices == true ) return $options[$option_name]['choices'];
-	else if( $current_options[$option_name] == '' ) return $options[$option_name]['default'];
+		'arrows_bkg_color' => array(
+			'name' => 'arrows_bkg_color',
+			'label' => 'Arrows Background Color',
+			'type' => 'color',
+			'default' => '#000',
+		),
+	)
+);
+
+function get_feat_option_value( $option_name ) {
+	global $rt_feat_settings;
+	foreach ($rt_feat_settings as $sections) {
+		foreach($sections as $section) {
+			if( $section['name'] == $option_name ) $default = $section['default'];
+		}
+	}
+	$current_options = get_option('rt_features');
+	if( $current_options[$option_name] == '' ) return $default;
 	else return $current_options[$option_name];
 }
 
-// DELETE ME
-function rt_get_feature_option( $option_name ) {
-	
-	$defaults = array(
-		'rt_feat_type' => 'Standard Slider',
-		'std_rt_feat_bkg_type' => 'Color',
-		'std_rt_feat_bkg' => '#fff',
-		'fw_rt_feat_bkg_1' => '#ccc',
-		'fw_rt_feat_bkg_2' => '#'
-	);
-	
-	$options = get_option( 'rt_features' );
-	
-	if( $options[$option_name] == '' ) {
-		return $defaults[$option_name];
+function get_feat_option_choices( $option_name ) {
+	global $rt_feat_settings;
+	foreach ($rt_feat_settings as $sections) {
+		foreach($sections as $section) {
+			if( $section['name'] == $option_name ) $choices = $section['choices'];
+		}
 	}
-	else {
-		return $options[$option_name];
-	}
-} 
+	return $choices;
+}
 
 ?>
