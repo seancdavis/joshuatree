b4773657ccf483b437dc491c49947b5fd7f6aa56
Sean C. Davis <scdavis41@gmail.com>
2013-03-04 21:45:57 -0500

    Bug fixes for Chrome. Added favicon. Still a few bugs to work out with IE.

--- header.php
+++ header.php
@@ -11,6 +11,7 @@
 <link rel="profile" href="http://gmpg.org/xfn/11" />
 <link rel="stylesheet" type="text/css" media="all" href="<?php bloginfo( 'stylesheet_url' ); ?>" />
 <link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>" />
+<link rel="shortcut icon" href="<?php echo get_template_directory_uri(); ?>/images/favicon.ico" />
  
 <?php
 	// Supports threaded comments     
--- library/content/social-media.php
+++ library/content/social-media.php
@@ -8,11 +8,11 @@ function rt_get_social_icons() {
 	$social_dir = get_template_directory_uri() . '/images/social-media/';
 	if( has_social_icons() ) {
 		$social_icons .= '<div id="social-icons-container" class="clearfix">';		
-		if( rt_get_option('rt_email') != '' ) $social_icons .= '<a href="' . rt_get_option('rt_email') . '" target="_blank"><img src="' . $social_dir.'email.png" /></a>';
-		if( rt_get_option('rt_facebook') != '' ) $social_icons .= '<a href="' . rt_get_option('rt_facebook') . '" target="_blank"><img src="' . $social_dir.'facebook.png" /></a>';		
-		if( rt_get_option('rt_twitter') != '' ) $social_icons .= '<a href="' . rt_get_option('rt_twitter') . '" target="_blank"><img src="' . $social_dir.'twitter.png" /></a>';	
-		if( rt_get_option('rt_linkedin') != '' ) $social_icons .= '<a href="' . rt_get_option('rt_linkedin') . '" target="_blank"><img src="' . $social_dir.'linkedin.png" /></a>';	
-		if( rt_get_option('rt_pinterest') != '' ) $social_icons .= '<a href="' . rt_get_option('rt_pinterest') . '" target="_blank"><img src="' . $social_dir.'pinterest.png" /></a>';		
+		if( rt_get_option('rt_email') != '' ) $social_icons .= '<a href="mailto:' . rt_get_option('rt_email') . '" target="_blank"><img src="' . $social_dir.'email-white.png" /></a>';
+		if( rt_get_option('rt_facebook') != '' ) $social_icons .= '<a href="' . rt_get_option('rt_facebook') . '" target="_blank"><img src="' . $social_dir.'facebook-white.png" /></a>';		
+		if( rt_get_option('rt_twitter') != '' ) $social_icons .= '<a href="' . rt_get_option('rt_twitter') . '" target="_blank"><img src="' . $social_dir.'twitter-white.png" /></a>';	
+		if( rt_get_option('rt_linkedin') != '' ) $social_icons .= '<a href="' . rt_get_option('rt_linkedin') . '" target="_blank"><img src="' . $social_dir.'linkedin-white.png" /></a>';	
+		if( rt_get_option('rt_pinterest') != '' ) $social_icons .= '<a href="' . rt_get_option('rt_pinterest') . '" target="_blank"><img src="' . $social_dir.'pinterest-white.png" /></a>';		
 		$social_icons .= '</div>';		
 	}
 	return $social_icons;
